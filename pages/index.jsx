2f492788afaaa97a0fc45533d94e67d4644be2ac
raffacreu <rafaeu.rocha87@gmail.com>
2023-01-05 18:36:01 -0300

    fix: zero on the left

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,13 +6,24 @@ import { useState } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  
   const [resultado, setResultado] = useState(0)
   
   function inputResultado(e){
     const input = e.target.value
-    setResultado( resultado + input )
-    //setResultado(valor)
+
+      if (resultado === 0) {
+        setResultado( input )
+          if (input == 0) {
+          alert(`Zeros à esquerda não possuem valor, tente um outro número`)
+          return setResultado(0)
+          }
+      }else{
+        setResultado( resultado + input )
+      }      
+  }
+
+  function clearResultado(e){
+    setResultado(0)
   }
 
   return (
@@ -24,43 +35,65 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"></link>
       </Head>
+
       <main>
         <div className='calculator-container'>
-        
-          <div><span className='screen-display'>{resultado}</span></div>
+
+          <span className='screen-display'>{resultado}</span>
+
           <div className='calculator-buttons'>
-            <tr>
-              <button data-clear-all>C</button>
-              <button data-delete><i className='fas fa-backspace'></i></button>
-              <button data-operator='x'>x</button>
-              <button data-operator='/' className='orange'>&#247;</button>
-            </tr>
 
-            <tr>
-              <button onClick={inputResultado} value={7} className='gray'>7</button>
-              <button onClick={inputResultado} value={8} className='gray'>8</button>
-              <button onClick={inputResultado} value={9} className='gray'>9</button>
-              <button data-operator='-' className='orange'>-</button>
-            </tr>
-
-            <tr>
-              <button onClick={inputResultado} value={4} className='gray'>4</button>
-              <button onClick={inputResultado} value={5} className='gray'>5</button>
-              <button onClick={inputResultado} value={6} className='gray'>6</button>
-              <button data-operator='+' className='orange'>+</button>
-            </tr>
-
-            <tr>
-              <button onClick={inputResultado} value={1} className='gray'>1</button>
-              <button onClick={inputResultado} value={2} className='gray'>2</button>
-              <button onClick={inputResultado} value={3} className='gray'>3</button>
-              <button data-equal className='orange'>=</button>
-            </tr>
-
-            <tr>
-              <button onClick={inputResultado} value={0} className='gray'>0</button>
-              <button data-number='.' className='gray'>.</button>
-            </tr>
+            <button className='darkGray' onClick={clearResultado} >AC</button>
+            <button className='darkGray' >+/-</button>
+            <button className='darkGray' >%</button>
+            <button className="blue"  value="/">
+              /
+            </button>
+            <button className="gray" onClick={inputResultado} value={7}>
+              7
+            </button>
+            <button className="gray" onClick={inputResultado} value={8}>
+              8
+            </button>
+            <button className="gray" onClick={inputResultado} value={9}>
+              9
+            </button>
+            <button className="blue"  value="X">
+              X
+            </button>
+            <button className="gray" onClick={inputResultado} value={4}>
+              4
+            </button>
+            <button className="gray" onClick={inputResultado} value={5}>
+              5
+            </button>
+            <button className="gray" onClick={inputResultado} value={6}>
+              6
+            </button>
+            <button className="blue"  value="-">
+              -
+            </button>
+            <button className="gray" onClick={inputResultado} value={1}>
+              1
+            </button>
+            <button className="gray" onClick={inputResultado} value={2}>
+              2
+            </button>
+            <button className="gray" onClick={inputResultado} value={3}>
+              3
+            </button>
+            <button className="blue" value="+">
+              +
+            </button>
+            <button className="gray" onClick={inputResultado} id='buttonZero' value={0}>
+              0
+            </button>
+            <button className="gray" onClick={inputResultado} value={"."}>
+              ,
+            </button>
+            <button className="blue" >
+              =
+            </button>
           </div>
         </div>
       </main>
